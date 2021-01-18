a0ab594bac355555cf64f387992d9bcf6f9dc762
Emil <emil@12kites.com>
2021-01-18 11:17:01 +0000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = grunt =>  {
+  const sass = require(sass)
+
+  grunt.initConfig({
+    sass: {
+
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-sass')
+
+  grunt.registerTask('name-the task',)
+}
